1f9de29b1b3e9ac296806e3ad3c357596971575c
official-ids <dimasik47k@gmail.com>
2026-07-27 16:44:51 +0400

    wb35

--- api/proxy.js
+++ api/proxy.js
@@ -77,7 +77,8 @@ const MODULE_PATHS = {
     JSON_STUDIO: '../json_studio',
     REVIEW: '../review',
     GRADE: '../grade',
-    SCRIPTBLOX: '../scriptblox'
+    SCRIPTBLOX: '../scriptblox',
+    RATES: '../rates'
 };
 
 /**
@@ -106,6 +107,8 @@ const reviewRouter = require(MODULE_PATHS.REVIEW);
 const gradeRouter = require(MODULE_PATHS.GRADE);
 
 const scriptbloxRouter = require(MODULE_PATHS.SCRIPTBLOX);
+
+const ratesRouter = require(MODULE_PATHS.RATES);
 
 // -----------------------------
 // Константы и Конфигурация
@@ -1280,6 +1283,7 @@ app.use('/api/json_studio', jsonStudioRouter);
 app.use('/api/review', reviewRouter);
 app.use('/api/grade', gradeRouter);
 app.use('/api/scriptblox', scriptbloxRouter);
+app.use('/api/rates', ratesRouter);
 
 // -----------------------------
 // Dynamic Page: Downloader
